38ce487748d3875d08fed3d51815753d7013e45d
Yassmina1 <110409586+Yassmina1@users.noreply.github.com>
2025-04-09 20:06:22 +0100

    Ajout des questionnaires coeur et rein + mise à jour Home.dart

--- lib/QuestionnaireDiabeteApp.dart
+++ lib/QuestionnaireDiabeteApp.dart
@@ -1,93 +1,72 @@
-import 'package:app/Home.dart';
 import 'package:flutter/material.dart';
-
 
 class QuestionnaireDiabeteApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Questionnaire Diabète',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
+      title: 'Diabetes Questionnaire',
       debugShowCheckedModeBanner: false,
-      home: QuestionnairePage(),
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: QuestionnaireScreen(),
     );
   }
 }
 
-class QuestionnairePage extends StatefulWidget {
+class QuestionnaireScreen extends StatefulWidget {
   @override
-  _QuestionnairePageState createState() => _QuestionnairePageState();
+  _QuestionnaireScreenState createState() => _QuestionnaireScreenState();
 }
 
-class _QuestionnairePageState extends State<QuestionnairePage> {
+class _QuestionnaireScreenState extends State<QuestionnaireScreen> {
+  int _currentQuestionIndex = 0;
+  int _yesCount = 0;
+  final Color _primaryColor = Color.fromARGB(255, 35, 111, 252);
+
   final List<String> _questions = [
-    "Avez-vous souvent soif ?",
-    "Urinez-vous fréquemment ?",
-    "Avez-vous une fatigue inhabituelle ?",
-    "Avez-vous des troubles de la vision ?",
-    "Avez-vous perdu du poids récemment sans raison ?",
-    "Avez-vous des infections fréquentes ?",
-    "Avez-vous des picotements ou engourdissements dans les mains ou les pieds ?",
-    "Avez-vous une cicatrisation lente des plaies ?",
-    "Avez-vous une peau sèche et qui démange ?",
-    "Avez-vous des antécédents familiaux de diabète ?",
+    "Do you experience frequent thirst?",
+    "Do you feel excessive hunger?",
+    "Do you have frequent urination?",
+    "Do you experience sudden weight loss?",
+    "Do you feel tired all the time?",
+    "Do you have blurry vision?",
+    "Do you heal wounds slowly?",
+    "Do you often have dry skin?",
+    "Do you have tingling or numbness in hands or feet?",
+    "Do you have high blood sugar levels?",
   ];
 
-  int _currentQuestionIndex = 0;
-  int _ouiCount = 0;
-  int _nonCount = 0;
-  double _probability = 0.0;
-
-  void _repondreOui() {
+  void _nextQuestion(bool isYes) {
     setState(() {
-      _ouiCount++;
-      _nextQuestion();
-    });
-  }
-
-  void _repondreNon() {
-    setState(() {
-      _nonCount++;
-      _nextQuestion();
-    });
-  }
-
-  void _nextQuestion() {
-    if (_currentQuestionIndex < _questions.length - 1) {
-      _currentQuestionIndex++;
-    } else {
-      // Calculer la probabilité après la dernière question
-      _probability = (_ouiCount / _questions.length) * 100;
-      if (_probability >= 50) {
-        // Naviguer vers la nouvelle page si la probabilité est >= 50%
-        Navigator.push(
-          context,
-          MaterialPageRoute(
-            builder: (context) => SecondPage(),
-          ),
-        );
+      if (isYes) _yesCount++;
+      if (_currentQuestionIndex < _questions.length - 1) {
+        _currentQuestionIndex++;
       } else {
         _showResultDialog();
       }
-    }
+    });
   }
 
   void _showResultDialog() {
+    double probability = (_yesCount / _questions.length) * 100;
     showDialog(
       context: context,
       builder: (BuildContext context) {
         return AlertDialog(
-          title: Text("Résultat"),
-          content: Text("Probabilité de diabète : ${_probability.toStringAsFixed(2)} %"),
+          title: Text("Result"),
+          content: Text(_getDiagnosisMessage(probability)),
           actions: [
             TextButton(
               onPressed: () {
                 Navigator.of(context).pop();
                 _resetQuestionnaire();
               },
-              child: Text("Recommencer"),
+              child: Text("Restart"),
+            ),
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: Text("Back to Home"),
             ),
           ],
         );
@@ -95,383 +74,135 @@ class _QuestionnairePageState extends State<QuestionnairePage> {
     );
   }
 
+  String _getDiagnosisMessage(double probability) {
+    if (probability >= 70) {
+      return "High risk of diabetes. Please consult a doctor.";
+    } else if (probability >= 40) {
+      return "Moderate risk. Monitor your symptoms.";
+    } else {
+      return "Low risk. Keep monitoring your health.";
+    }
+  }
+
   void _resetQuestionnaire() {
     setState(() {
       _currentQuestionIndex = 0;
-      _ouiCount = 0;
-      _nonCount = 0;
-      _probability = 0.0;
+      _yesCount = 0;
     });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Pre-consultation'),
-        backgroundColor: Colors.blue[900],
-      ),
-      backgroundColor: Colors.blue, // Fond bleu clair
-      body: Center(
-        child: Container(
-          width: 300, // Largeur du contenu
-          padding: const EdgeInsets.all(16.0),
-          decoration: BoxDecoration(
-            color: Colors.white, // Fond blanc pour le contenu
-            borderRadius: BorderRadius.circular(10), // Bordures arrondies
-            boxShadow: [
-              BoxShadow(
-                color: Colors.black.withOpacity(0.1),
-                spreadRadius: 5,
-                blurRadius: 7,
-                offset: Offset(0, 3), // Ombre légère
-              ),
-            ],
-          ),
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: <Widget>[
-              Text(
-                _questions[_currentQuestionIndex],
-                style: TextStyle(fontSize: 18),
-                textAlign: TextAlign.center,
-              ),
-              SizedBox(height: 20),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: <Widget>[
-                  ElevatedButton(
-                    onPressed: _repondreOui,
-                    child: Text('Oui'),
+      backgroundColor: Colors.white,
+      body: Stack(
+        children: [
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              _buildProgressIndicator(),
+              const SizedBox(height: 30),
+              Image.asset("images/diabet.png", height: 100),
+              const SizedBox(height: 20),
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 30),
+                child: Text(
+                  "Let's start checking your insulin level by some basic questions",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black,
                   ),
-                  ElevatedButton(
-                    onPressed: _repondreNon,
-                    child: Text('Non'),
-                  ),
-                ],
-              ),
-              SizedBox(height: 20),
-              Text(
-                "Question ${_currentQuestionIndex + 1} sur ${_questions.length}",
-                style: TextStyle(fontSize: 14),
-                textAlign: TextAlign.center,
-              ),
-            ],
-          ),
-        ),
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        items: const <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            label: 'Home',
-          ),
-
-          BottomNavigationBarItem(
-            icon: Icon(Icons.history),
-            label: 'History',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.person),
-            label: 'Profil',
-          ),
-        ],
-        currentIndex: 2,
-        selectedItemColor: Colors.blue[900],
-        onTap: (int index) {
-          switch (index) {
-            case 0:
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => HomeScreen()),
-              );
-              break;
-            case 1:
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => ProfilPage()),
-              );
-              break;
-            case 2:
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => HistoryPage()),
-              );
-              break;
-          }
-        },
-      ),
-    );
-  }
-}
-
-
-// Nouvelle page avec deux champs
-class SecondPage extends StatelessWidget {
-  final TextEditingController _field1Controller = TextEditingController();
-  final TextEditingController _field2Controller = TextEditingController();
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Informations supplémentaires'),
-        backgroundColor: Colors.blue[900],
-
-      ),
-      backgroundColor: Colors.blue, // Fond bleu clair
-      body: Center(
-        child: Container(
-          width: 300, // Largeur du contenu
-          padding: const EdgeInsets.all(16.0),
-          decoration: BoxDecoration(
-            color: Colors.white, // Fond blanc pour le contenu
-            borderRadius: BorderRadius.circular(10), // Bordures arrondies
-            boxShadow: [
-              BoxShadow(
-                color: Colors.black.withOpacity(0.1),
-                spreadRadius: 5,
-                blurRadius: 7,
-                offset: Offset(0, 3), // Ombre légère
-              ),
-            ],
-          ),
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: <Widget>[
-              TextField(
-                controller: _field1Controller,
-                decoration: InputDecoration(
-                  labelText: 'Champ 1',
-                  border: OutlineInputBorder(),
                 ),
               ),
-              SizedBox(height: 20),
-              TextField(
-                controller: _field2Controller,
-                decoration: InputDecoration(
-                  labelText: 'Champ 2',
-                  border: OutlineInputBorder(),
+              const SizedBox(height: 40),
+              Container(
+                padding: EdgeInsets.all(10),
+                decoration: BoxDecoration(
+                  color: _primaryColor,
+                  borderRadius: BorderRadius.circular(20),
+                ),
+                child: Text(
+                  "Question ${_currentQuestionIndex + 1}",
+                  style: TextStyle(
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                  ),
                 ),
               ),
-              SizedBox(height: 20),
-              ElevatedButton(
-                onPressed: () {
-                  // Naviguer vers la page de résultat finale
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => ResultPage(
-                        field1: _field1Controller.text,
-                        field2: _field2Controller.text,
-                      ),
-                    ),
-                  );
-                },
-                child: Text('Analyser'),
+              const SizedBox(height: 20),
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 30),
+                child: Text(
+                  _questions[_currentQuestionIndex],
+                  textAlign: TextAlign.center,
+                  style: TextStyle(fontSize: 18, color: Colors.black),
+                ),
               ),
+              const SizedBox(height: 30),
+              _buildAnswerButtons(),
             ],
           ),
-        ),
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        items: const <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            label: 'Home',
-          ),
-
-          BottomNavigationBarItem(
-            icon: Icon(Icons.history),
-            label: 'History',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.person),
-            label: 'Profil',
+          Positioned(
+            bottom: 10,
+            left: 0,
+            right: 0,
+            child: Center(child: Image.asset("images/img.png", height: 60)),
           ),
         ],
-        currentIndex: 1,
-        selectedItemColor: Colors.blue[900],
-        onTap: (int index) {
-          switch (index) {
-            case 0:
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => HomePage()),
-              );
-              break;
-            case 1:
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => ProfilPage()),
-              );
-              break;
-            case 2:
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => HistoryPage()),
-              );
-              break;
-          }
-        },
       ),
     );
   }
-}
 
-// Page de résultat finale
-class ResultPage extends StatelessWidget {
-  final String field1;
-  final String field2;
-
-  ResultPage({required this.field1, required this.field2});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Résultat Final'),
-        backgroundColor: Colors.blue[900],
-
-      ),
-      backgroundColor: Colors.blue, // Fond bleu clair
-      body: Center(
-        child: Container(
-          width: 300, // Largeur du contenu
-          padding: const EdgeInsets.all(16.0),
+  Widget _buildProgressIndicator() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: List.generate(_questions.length, (index) {
+        return AnimatedContainer(
+          duration: Duration(milliseconds: 300),
+          margin: EdgeInsets.symmetric(horizontal: 5),
+          width: _currentQuestionIndex == index ? 15 : 10,
+          height: _currentQuestionIndex == index ? 15 : 10,
           decoration: BoxDecoration(
-            color: Colors.white, // Fond blanc pour le contenu
-            borderRadius: BorderRadius.circular(10), // Bordures arrondies
-            boxShadow: [
-              BoxShadow(
-                color: Colors.black.withOpacity(0.1),
-                spreadRadius: 5,
-                blurRadius: 7,
-                offset: Offset(0, 3), // Ombre légère
-              ),
-            ],
+            color:
+                _currentQuestionIndex >= index
+                    ? _primaryColor
+                    : Colors.grey[300],
+            shape: BoxShape.circle,
           ),
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: <Widget>[
-              Text(
-                'Résultat de l\'analyse',
-                style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-                textAlign: TextAlign.center,
-              ),
-              SizedBox(height: 20),
-              Text(
-                'hhhhhhhhhhhhhhhhhhh',
-                style: TextStyle(fontSize: 18),
-                textAlign: TextAlign.center,
-              ),
-              SizedBox(height: 10),
-              Text(
-                'Pourcentage %',
-                style: TextStyle(fontSize: 18),
-                textAlign: TextAlign.center,
-              ),
-              SizedBox(height: 20),
-              ElevatedButton(
-                onPressed: () {
-                  // Retourner à la page d'accueil
-                  Navigator.popUntil(context, (route) => route.isFirst);
-                },
-                child: Text('Recommencer'),
-              ),
-            ],
-          ),
-        ),
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        items: const <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            label: 'Home',
-          ),
-
-          BottomNavigationBarItem(
-            icon: Icon(Icons.history),
-            label: 'History',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.person),
-            label: 'Profil',
-          ),
-        ],
-        currentIndex: 2,
-        selectedItemColor: Colors.blue[900],
-        onTap: (int index) {
-          switch (index) {
-            case 0:
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => HomeScreen()),
-              );
-              break;
-            case 1:
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => ProfilPage()),
-              );
-              break;
-            case 2:
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => HistoryPage()),
-              );
-              break;
-          }
-        },
-      ),
+        );
+      }),
     );
   }
-}
 
-// Pages supplémentaires
-class HomePage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Home'),
-        backgroundColor: Colors.blue[900],
-      ),
-      body: Center(
-        child: Text('Page d\'accueil'),
-      ),
+  Widget _buildAnswerButtons() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        _buildAnswerButton("Yes", Colors.blue, Colors.white, Icons.check, true),
+        const SizedBox(width: 20),
+        _buildAnswerButton("No", Colors.red, Colors.white, Icons.close, false),
+      ],
     );
   }
-}
 
-class ProfilPage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Profil'),
-        backgroundColor: Colors.blue[900],
-      ),
-      body: Center(
-        child: Text('Page de profil'),
-      ),
-    );
-  }
-}
-
-class HistoryPage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('History'),
-        backgroundColor: Colors.blue[900],
-      ),
-      body: Center(
-        child: Text('Page d\'historique'),
+  Widget _buildAnswerButton(
+    String text,
+    Color bgColor,
+    Color textColor,
+    IconData icon,
+    bool isYes,
+  ) {
+    return ElevatedButton.icon(
+      onPressed: () => _nextQuestion(isYes),
+      icon: Icon(icon, color: textColor),
+      label: Text(text, style: TextStyle(fontSize: 18, color: textColor)),
+      style: ElevatedButton.styleFrom(
+        backgroundColor: bgColor,
+        padding: EdgeInsets.symmetric(horizontal: 40, vertical: 15),
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
       ),
     );
   }
